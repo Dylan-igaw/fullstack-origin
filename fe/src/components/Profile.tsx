a87c2ac95fb3deb5bffa6e622fe82b553e466985
Dylan-igaw <dylan.kim@igaworks.com>
2020-02-13 14:08:17 +0900

    feedback update

--- fe/src/components/Profile.tsx
+++ fe/src/components/Profile.tsx
@@ -3,7 +3,44 @@ import '../css/Profile.css';
 import {observer} from "mobx-react";
 import {action, observable} from "mobx";
 import {Link} from "react-router-dom";
+import validateLogin from "./ValidateLogin";
 
+
+interface renderItem {
+    data: any,
+}
+
+class RenderList extends React.Component<renderItem> {
+    render() {
+        if(this.props.data === null) return '';
+        return <ul>
+            <li>이름 : {this.props.data.name}</li>
+            <li>나이 : {this.props.data.age}</li>
+            <li>보유차량
+                <ul>
+                    <li>
+                        {this.props.data.cars[0].name}
+                    </li>
+                    <ul>
+                        <li>모델 : {this.props.data.cars[0].models.join(', ')}</li>
+                    </ul>
+                    <li>
+                        {this.props.data.cars[1].name}
+                    </li>
+                    <ul>
+                        <li>모델 : {this.props.data.cars[1].models.join(', ')}</li>
+                    </ul>
+                    <li>
+                        {this.props.data.cars[2].name}
+                    </li>
+                    <ul>
+                        <li>모델 : {this.props.data.cars[2].models.join(', ')}</li>
+                    </ul>
+                </ul>
+            </li>
+        </ul>;
+    }
+}
 
 @observer
 export default class Profile extends React.Component<any> {
@@ -12,37 +49,12 @@ export default class Profile extends React.Component<any> {
     private loginId: string | null = sessionStorage.getItem("id");
 
     @observable
-    private profileListHtml: object | string = '';
+    private profileList: any = null;
 
     @action
-    makeProfileList = (data: any) => {
-        this.profileListHtml = <ul>
-            <li>이름 : {data.name}</li>
-            <li>나이 : {data.age}</li>
-            <li>보유차량
-                <ul>
-                    <li>
-                        {data.cars[0].name}
-                    </li>
-                    <ul>
-                        <li>모델 : {data.cars[0].models.join(', ')}</li>
-                    </ul>
-                    <li>
-                        {data.cars[1].name}
-                    </li>
-                    <ul>
-                        <li>모델 : {data.cars[1].models.join(', ')}</li>
-                    </ul>
-                    <li>
-                        {data.cars[2].name}
-                    </li>
-                    <ul>
-                        <li>모델 : {data.cars[2].models.join(', ')}</li>
-                    </ul>
-                </ul>
-            </li>
-        </ul>;
-    }
+    insertJsonData = (data: any) => {
+        this.profileList = data;
+    };
 
     loadProfile = () => {
         const url: string = 'http://localhost:3001/profile';
@@ -59,32 +71,28 @@ export default class Profile extends React.Component<any> {
             .then((parserJson) => {
                 if (parserJson.rs) {
                     console.log(parserJson.message);
-                    console.log(parserJson.data);
-                    this.makeProfileList(parserJson.data);
+                    this.insertJsonData(parserJson.data);
                 } else {
+                    this.insertJsonData(null);
                     alert(parserJson.message);
                 }
             });
     };
 
     render() {
-        if (this.loginId === null) {
-            return <div className="Game">
-                <div className="Game-header">
-                    <Link to={"/"} className={"link"}>로그인 후 이용해 주세요.</Link>
-                </div>
-            </div>;
-        } else {
-            return <div className="profile">
-                <>
-                    <button className="load-btn" onClick={this.loadProfile}>
-                        Load Profile
-                    </button>
-                </>
-                <div className="profile-list">
-                    {this.profileListHtml}
-                </div>
-            </div>;
-        }
+        if (validateLogin()) return <div className="profile">
+            <Link to={"/"} className={"link"}>로그인 후 이용해 주세요.</Link>
+        </div>;
+
+        return <div className="profile">
+            <>
+                <button className="load-btn" onClick={this.loadProfile}>
+                    Load Profile
+                </button>
+            </>
+            <div className="profile-list">
+                <RenderList data={this.profileList}/>
+            </div>
+        </div>
     }
 }
